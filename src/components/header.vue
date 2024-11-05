e21072163b1e0420d11e111ce7a757eb6ecd0bb3
lucas <lucasdesouzasouza57@gmail.com>
2024-11-05 10:05:19 -0300

    FEAT: Header

---
+++ src/components/header.vue
@@ -0,0 +1,86 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+
+
+
+</script>
+
+<template>
+  <header class="header">
+    <div class="logo">Logo</div>
+    <div class="titulo">Semeando o Futuro</div>
+    <nav>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">Gráfico</a></li>
+        <li><a href="#services">Serviços</a></li>
+        <li><a href="#contact">Contato</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #333;
+  color: white;
+}
+
+.logo {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+
+
+.titulo {
+  font-size: 2rem; /* Aumente o tamanho da fonte aqui */
+  font-weight: bold;
+}
+
+nav ul {
+  display: flex;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+nav ul li {
+  margin-left: 1rem;
+}
+
+nav ul li a {
+  color: white;
+  text-decoration: none;
+}
+
+/* Estilos para telas menores */
+@media (max-width: 768px) {
+  .header {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  nav {
+    width: 100%;
+    margin-top: 1rem;
+  }
+
+  nav ul {
+    flex-direction: column;
+  }
+
+  nav ul li {
+    margin: 0.5rem 0;
+  }
+}
+</style>
+
---
+++ src/views/teste.vue
@@ -0,0 +1,13 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+import Head from '@/components/header.vue'
+</script>
+
+<template>
+
+<Head />
+
+
+</template>
+
+<style scoped></style>
